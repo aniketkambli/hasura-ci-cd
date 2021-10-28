1982b0de7552e9159b651ace4cf1b5fdfa23156c
Shweta Mahavarkar <vagrant@vagrant.vm>
2021-10-28 09:15:25 +0000

    Trial project setup

---
+++ new2510/proj2810/proj2-2810/migrations/default/1635402135214_run_sql_migration/down.sql
@@ -0,0 +1,26 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- SET check_function_bodies = false;
+-- CREATE SCHEMA userschema;
+-- CREATE TABLE userschema.leaves (
+--     "leaveId" integer NOT NULL,
+--     type text NOT NULL,
+--     date date
+-- );
+-- CREATE SEQUENCE userschema."leaves_leaveId_seq"
+--     AS integer
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+-- ALTER SEQUENCE userschema."leaves_leaveId_seq" OWNED BY userschema.leaves."leaveId";
+-- CREATE TABLE userschema."user" (
+--     "userId" integer NOT NULL,
+--     name text NOT NULL
+-- );
+-- ALTER TABLE ONLY userschema.leaves ALTER COLUMN "leaveId" SET DEFAULT nextval('userschema."leaves_leaveId_seq"'::regclass);
+-- ALTER TABLE ONLY userschema.leaves
+--     ADD CONSTRAINT leaves_pkey PRIMARY KEY ("leaveId");
+-- ALTER TABLE ONLY userschema."user"
+--     ADD CONSTRAINT user_pkey PRIMARY KEY ("userId");
---
+++ new2510/proj2810/proj2-2810/migrations/default/1635402841603_create_table_userschema_holiday/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "userschema"."holiday" ("id" integer NOT NULL, "name" text NOT NULL, PRIMARY KEY ("id") , UNIQUE ("id"));
